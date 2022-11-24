b7bd1f724f89fb836e561b2d6fce9a442c41e5c5
ChristianCantuA <christian.alex.cantu@gmail.com>
2022-11-23 21:55:09 -0600

    layout
    begun making layout, dropdown is giving me issues

---
+++ scripts.js
@@ -0,0 +1,8 @@
+console.log('what is love');
+
+const btn = document.querySelector('#button');
+
+btn.addEventListener('click', (event) => {
+    console.log('button was clicked');
+    alert("you done goofed")
+});
